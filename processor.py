e6e499b40cb4490cdeff00f6264fc6bd0147e496
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-08-11 10:55:19 +0300

    Update processor.py

--- processor.py
+++ processor.py
@@ -5,19 +5,17 @@ lines=f.readlines()
 username=lines[0]
 password=lines[1]
 
-def download_xml(self, url, product, out_path, login):
+print(username)
+print(password)
+
+def download_xml(product, out_path):
     user_name = username
     user_password = password
+    scihub_url = "https://scihub.copernicus.eu/dhus/search?q="
+    command = "wget --no-verbose --no-check-certificate --user={user} --password={pwd} --output-document={out}"\
+                  " {url}".format(user=user_name, pwd=user_password, out=out_path, url=(scihub_url + product))
 
-    command = "wget --no-verbose --no-check-certificate --output-document={out} {url}".format(out=out_path, url=url + product)
-    if login:
-        command = "wget --no-verbose --no-check-certificate --user={user} --password={pwd} --output-document={out}"\
-                  " {url}".format(user=user_name, pwd=user_password, out=out_path, url=(url + product))
-
-    self.info("Downloading product as " + command)
-    error_code, error_msg = utilities.execute(command)
-    if error_code != 0:
-        raise RuntimeError("Failed to download product {}. Error code: {}, error message:\n{}"
-                           .format(product, error_code, error_msg))
-    if login:
-        time.sleep(1.5)  # scihub does not allow too frequent queries; therefore wait a bit before a new query
+    print("Downloading product as " + command)
+    time.sleep(1.5)  # scihub does not allow too frequent queries; therefore wait a bit before a new query
+        
+download_xml("S2*MSIL2A*202006*T35VMC*","proov.xml")
