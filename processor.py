91811ab92d562e0363ab09538565001c896dbd3c
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-08-11 11:50:42 +0300

    Update processor.py

--- processor.py
+++ processor.py
@@ -2,15 +2,6 @@ import os
 import time
 from pathlib import Path
 
-f=open("login.txt","r")
-lines=f.readlines()
-username=lines[0].rstrip()
-password=lines[1].rstrip()
-f.close()
-
-print(username)
-print(password)
-
 def download_xml(product, out_path):
     user_name = username
     user_password = password
@@ -23,14 +14,35 @@ def download_xml(product, out_path):
     time.sleep(1.5)  # scihub does not allow too frequent queries; therefore wait a bit before a new query
     
 def read_xml(out_path):
-    product_list=[]
+    current_list=[]
     txt = Path(out_path).read_text()
     products=txt.split("<title>")
     for i in range(2,len(products)):
         product=products[i].split("</title>")[0]
         product_list.append(product)
-    print(len(product_list))
-    print(product_list)
-        
-download_xml("S2*MSIL2A*202006*T35VMC*","proov.xml")
-read_xml("proov.xml")
+    return current_list
+
+f=open("login.txt","r")
+lines=f.readlines()
+username=lines[0].rstrip()
+password=lines[1].rstrip()
+f.close()
+
+print(username)
+print(password)
+
+year="2020"
+place="T35VMC"
+months=["11","10","09","08","07","06","05","04"]
+active_months=["11","10","09","08","07"]
+passive_months=["06","05","04"]
+
+product_list=[]
+
+for month in months:
+    download_xml("S2*MSIL2A*"+year+"*"+place+"*",month+".xml")
+    month_list=read_xml(month+".xml")
+    for product in month_list:
+        product_list.append(product)
+
+print(product_list)
