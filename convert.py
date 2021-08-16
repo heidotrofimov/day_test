552248f23e0ec958716863a5e21c7df88d99d306
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-08-16 09:53:42 +0300

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,19 +1,19 @@
 import os
 
-os.system("mkdir T35VMC_2")
+os.system("mkdir T35VMC_2020_2")
 
-os.system("mkdir T35VMC_2/clear_images")
+os.system("mkdir T35VMC_2020_2/clear_images")
 
-os.system("mkdir T35VMC_2/target_images")
+os.system("mkdir T35VMC_2020_2/target_images")
 
-for dire in os.listdir("T35VMC/target_images"):
-  f=open("T35VMC_2/target_images/"+dire+".txt","w")
-  for filename in os.listdir("T35VMC/target_images/"+dire):
+for dire in os.listdir("T35VMC_2020/target_images"):
+  f=open("T35VMC_2020_2/target_images/"+dire+".txt","w")
+  for filename in os.listdir("T35VMC_2020/target_images/"+dire):
     f.write(filename.split(".")[0]+"\n")
   f.close()
   
-for dire in os.listdir("T35VMC/clear_images"):
-  f=open("T35VMC_2/clear_images/"+dire+".txt","w")
-  for filename in os.listdir("T35VMC/target_images/"+dire):
+for dire in os.listdir("T35VMC_2020/clear_images"):
+  f=open("T35VMC_2020_2/clear_images/"+dire+".txt","w")
+  for filename in os.listdir("T35VMC_2020/target_images/"+dire):
     f.write(filename.split(".")[0]+"\n")
   f.close()
