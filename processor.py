2af91b6019ed1621a57da2b47e6e5b013915a45d
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-09-02 14:05:47 +0300

    Update processor.py

--- processor.py
+++ processor.py
@@ -105,7 +105,7 @@ def tile_clear_image(im_S2,name,where,pr):
                     where.write(str(i)+"_"+str(j)+"\n")
     if(im_S2.width>tiles_x*tile_size):
         for j in range(0,tiles_y):
-            mask_tile=mask_o.crop((mask.width-tile_size,j*tile_size,mask.width,tile_size*(j+1)))
+            mask_tile=mask_o.crop((mask_o.width-tile_size,j*tile_size,mask_o.width,tile_size*(j+1)))
             mask=mask_tile.load()
             polluted_pixels=0
             for k in range(mask_tile.width):
@@ -118,7 +118,7 @@ def tile_clear_image(im_S2,name,where,pr):
                     where.write(str(tiles_x)+"_"+str(j)+"\n")
     if(im_S2.height>tiles_y*tile_size):
         for i in range(0,tiles_x):
-            mask_tile=mask_o.crop((i*tile_size,mask.height-tile_size,tile_size*(i+1),mask.height))
+            mask_tile=mask_o.crop((i*tile_size,mask_o.height-tile_size,tile_size*(i+1),mask_o.height))
             mask=mask_tile.load()
             polluted_pixels=0
             for k in range(mask_tile.width):
@@ -130,7 +130,7 @@ def tile_clear_image(im_S2,name,where,pr):
                 if(check_data(RGB_tile)):
                     where.write(str(i)+"_"+str(tiles_y)+"\n")
     if(im_S2.height>tiles_y*tile_size and im_S2.width>tiles_x*tile_size):
-        mask_tile=mask_o.crop((mask.width-tile_size,mask.height-tile_size,mask.width,mask.height))
+        mask_tile=mask_o.crop((mask_o.width-tile_size,mask_o.height-tile_size,mask_o.width,mask_o.height))
         mask=mask_tile.load()
         polluted_pixels=0
         for k in range(mask_tile.width):
